aec0edcd839d76a7f750ad340befdd03850ad15f
Charels Bruno <charlesbrcosta@gmail.com>
2023-09-29 21:22:24 -0300

    ajuste do script.js e do README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# dev-links
+# darkMode e lightMode - devlinks
+## Tecnologias utilizadas
+
+![Badge Marking HTML5](https://img.shields.io/badge/Marking-HTML5-orange)
+![Badge Style CSS](https://img.shields.io/badge/Style-CSS-blue)
+![Badge Front/BackEnd JavaScript](https://shields.io/badge/Front/BackEnd-JavaScript-yellow)
+![Badge IDE VSCode](https://img.shields.io/badge/IDE-VSCode-blue)
+
+<h3>O projeto <strong>devlinks</strong> tem como objetivo, mostrar um pouco sobre o conceito de darkMode, lightMode e localStorage.</h3>
+<h3>O devlinks foi dividido em três partes: </h3>
+
+  - `index.html` : fica a estrutura (visual) do projeto;
+  - `style.css`  : toda a parte de estilização (cor, formatação etc.);
+  - `script.js`  : regra da funcionalidade para o darkMode, lightMode e localStorage 
+
+<h3>Funcionamento do projeto devlinks</h4>
+
+<p>Para a visualização e o funcionamento do projeto <strong>devlinks</strong>, basta abrir o arquivo index.html. Caso esteja utilizado o Visual Studio Code você pode instalar a <strong>extensão live server.</strong> Após a instalação clique com o botão direito em cima do <strong>index.html</strong> e escolher a opção <strong>Open with live server.</strong></p>
+
+<h2>Tela do devlinks</h2>
+<img src="./assets/tela-devlinks.png">
+
+
--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-function toggleMode(){
-  const html = document.documentElement
-  html.classList.toggle("light")
+// Verifica o valor no localStorage e aplica/remove a class "light"
+function toggleLightMode() {
+  const element = document.body
 
-   const img = document.querySelector('#profile img')
+  //Verifica se a class light ja esta aplicada ao elemento
+  const isLightMode = element.classList.contains("light")
 
-   if(html.classList.contains('light')){
-    img.setAttribute('src', './assets/avatar-light.png')
-    img.setAttribute(
-      'alt',
-      'Mike Bryto usando oculos escuro, sorrindo e de camisa preta com fundo de cor gradiente azul com roxo'
-    )
-   }else{
-    img.setAttribute('src', './assets/avatar.png')
-    img.setAttribute(
-      'alt',
-      'Mike Bryto usando oculos, sorrindo e de camisa preta com fundo de cor gradiente azul com roxo'
-    )
-   }
+  // Verifica se não está em modo light, adicione a classe "light" e armazene no localStorage
+  if (!isLightMode) {
+    element.classList.add("light")
+    localStorage.setItem("isLightMode", "true")
+  } else { // Remove a classe "light" e do localStorage
+    element.classList.remove("light")
+    localStorage.removeItem("isLightMode")
+  }
 }
+// verifica o valor no localStorage ao carregar a página
+function checkLightModeOnLoad() {
+  const isLightMode = localStorage.getItem('isLightMode') === "true"
+  if(isLightMode) {
+    const element = document.body
+    element.classList.add("light")
+  }
+}
+// Chame a função de verificação no carregamento da página
+checkLightModeOnLoad()
+
+// function toggleMode(){
+//   const html = document.documentElement
+//   html.classList.toggle("light")
+
+//    const img = document.querySelector('#profile img')
+
+//    if(html.classList.contains('light')){
+//     img.setAttribute('src', './assets/avatar-light.png')
+//     img.setAttribute(
+//       'alt',
+//       'Mike Bryto usando oculos escuro, sorrindo e de camisa preta com fundo de cor gradiente azul com roxo'
+//     )
+//    }else{
+//     img.setAttribute('src', './assets/avatar.png')
+//     img.setAttribute(
+//       'alt',
+//       'Mike Bryto usando oculos, sorrindo e de camisa preta com fundo de cor gradiente azul com roxo'
+//     )
+//    }
+// }
